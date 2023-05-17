4f7d462e8d143440f06be35f9d5cd581416b6018
kaito01234 <kaito_ogura@yayoi-kk.co.jp>
2023-05-17 12:11:57 +0900

    init

---
+++ pdf-configs/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  stylesheet: "https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.10.0/github-markdown.min.css",
+  body_class: "markdown-body",
+  marked_options: {
+    headerIds: false,
+    smartypants: true,
+  },
+  pdf_options: {
+    format: "A4",
+    margin: "30mm 20mm",
+    printBackground: true,
+  },
+  stylesheet_encoding: "utf-8",
+};
